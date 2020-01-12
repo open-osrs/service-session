787c34003c687f4c99f003e273ac5c1f1a55b0b2
TheRealNull <TheRealNull@gmail.com>
2020-01-12 04:40:37 -0500

    session: add shadowJar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.github.johnrengelman.shadow") version "5.2.0"
     java
 }
 
@@ -6,6 +7,7 @@ group = "com.openosrs"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
@@ -15,6 +17,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.2.2.RELEASE")
     implementation("org.projectlombok:lombok:1.18.10")
     testCompile("junit", "junit", "4.12")
+}
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "service.SpringBootWebApplication"
+    }
 }
 
 configure<JavaPluginConvention> {
